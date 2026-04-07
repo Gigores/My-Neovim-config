4f132d134ff56553ec4d78a66dd99223f14f3b89
Pricklen <igorsamodumskij@gmail.com>
2026-04-07 19:12:40 +0300

    update markdown config

--- lua/core/md.lua
+++ lua/core/md.lua
@@ -2,18 +2,36 @@ vim.pack.add {
 	{ src = "https://github.com/nvim-mini/mini.nvim" },
 	{ src = "https://github.com/MeanderingProgrammer/render-markdown.nvim" }
 }
+vim.treesitter.language.register('markdown', 'vimwiki')
 local markdown = require "render-markdown"
 markdown.setup {
+	indent = {
+		enabled = true
+	},
+	heading = {
+		border = true
+	},
+	code = {
+		style = "normal",
+		border = "thick"
+	},
 	completions = { lsp = { enabled = true } },
 	file_types = { 'markdown', 'vimwiki' },
 	checkbox = {
 		checked = {
-			icon = "  "
+			icon = " "
 		},
 		unchecked = {
-			icon = "  "
+			icon = " "
 		},
-		custom = {}
+		custom = {
+			in_progress = {
+				raw = "[/]",
+				rendered = " ",
+				highlight = 'DiagnosticWarn',
+				scope_highlight = nil
+			}
+		}
 	}
 }
 
