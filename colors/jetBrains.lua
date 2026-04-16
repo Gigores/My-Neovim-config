4eca5416dbeed88e7feb28d420e4d3d4479642a0
Pricklen <igorsamodumskij@gmail.com>
2026-04-16 16:23:15 +0300

    add jetBrains colorscheme

---
+++ colors/jetBrains.lua
@@ -0,0 +1,126 @@
+local COLORS = {
+	bg = "#191A1C",
+	fg = "#BCBEC4",
+	cursorline = "#1F2024",
+	selection  = "#214283",
+	linenr     = "#4B5059",
+	comment    = "#7A7E85",
+	whitespace = "#2F333A",
+
+	keyword  = "#CF8E6D",
+	Function = "#BCBEC4",
+	string   = "#6AAB73",
+	constant = "#C77DBB",
+	type     = "#CF8E6D",
+	number   = "#2AACB8",
+	boolean  = "#CF8E6D",
+	operator = "#BCBEC4",
+	variable = "#BCBEC4",
+	func_dec = "#56A8F5",
+	metadata = "#B3AE60",
+
+	cursor     = "#CED0D6",
+	visual     = "#214283",
+	search     = "#2D543F",
+	statusline = "#393B40",
+	menu_bg    = "#26272A",
+	menu_sel   = "#36383D",
+	fold       = "#393B40",
+	split      = "#393B40",
+
+	diag_error = "#D64D5B",
+	diag_warn  = "#C29E4A",
+	diag_info  = "#5E5339",
+	diag_hint  = "#1582A4"
+}
+
+local fields = {
+	-- base UI
+	["Normal"]       = { fg = COLORS.fg, bg = COLORS.bg },
+	["NormalFloat"]  = { fg = COLORS.fg, bg = COLORS.menu_bg },
+	["CursorLine"]   = { bg = COLORS.cursorline },
+	["CursorLineNr"] = { fg = COLORS.fg, bold = true },
+	["LineNr"]       = { fg = COLORS.linenr },
+	["Cursor"]       = { fg = COLORS.bg, bg = COLORS.cursor },
+	["Visual"]       = { bg = COLORS.visual },
+	["Search"]       = { bg = COLORS.search },
+	["IncSearch"]    = { bg = COLORS.selection, bold = true },
+	["Whitespace"]   = { fg = COLORS.whitespace },
+
+	["StatusLine"]   = { fg = COLORS.fg, bg = COLORS.statusline },
+	["StatusLineNC"] = { fg = COLORS.linenr, bg = COLORS.statusline },
+	["VertSplit"]    = { fg = COLORS.split },
+	["WinSeparator"] = { fg = COLORS.split },
+
+	["Pmenu"]        = { fg = COLORS.fg, bg = COLORS.menu_bg },
+	["PmenuSel"]     = { fg = COLORS.fg, bg = COLORS.menu_sel },
+	["PmenuSbar"]    = { bg = COLORS.fold },
+	["PmenuThumb"]   = { bg = COLORS.linenr },
+
+	["Folded"]       = { fg = COLORS.comment, bg = COLORS.fold },
+	["FoldColumn"]   = { fg = COLORS.linenr, bg = COLORS.bg },
+
+	-- syntax
+	["Comment"]    = { fg = COLORS.comment, italic = true },
+	["Keyword"]    = { fg = COLORS.keyword },
+	["Function"]   = { fg = COLORS.Function },
+	["String"]     = { fg = COLORS.string },
+	["Constant"]   = { fg = COLORS.constant },
+	["Type"]       = { fg = COLORS.type },
+	["Number"]     = { fg = COLORS.number },
+	["Boolean"]    = { fg = COLORS.boolean },
+	["Operator"]   = { fg = COLORS.operator },
+	["Identifier"] = { fg = COLORS.variable },
+	["Delimiter"]  = { fg = COLORS.fg },
+
+	-- common extras
+	["Statement"]  = { fg = COLORS.keyword },
+	["Conditional"]= { fg = COLORS.keyword },
+	["Repeat"]     = { fg = COLORS.keyword },
+	["Exception"]  = { fg = COLORS.keyword },
+	["PreProc"]    = { fg = COLORS.constant },
+	["Special"]    = { fg = COLORS.constant },
+
+	-- diagnostics
+	["DiagnosticError"] = { fg = COLORS.diag_error },
+	["DiagnosticWarn"]  = { fg = COLORS.diag_warn },
+	["DiagnosticInfo"]  = { fg = COLORS.diag_info },
+	["DiagnosticHint"]  = { fg = COLORS.diag_hint },
+
+	["DiagnosticUnderlineError"] = { undercurl = true, sp = COLORS.diag_error },
+	["DiagnosticUnderlineWarn"]  = { undercurl = true, sp = COLORS.diag_warn },
+	["DiagnosticUnderlineInfo"]  = { undercurl = true, sp = COLORS.diag_info },
+	["DiagnosticUnderlineHint"]  = { undercurl = true, sp = COLORS.diag_hint },
+	["DiagnosticDeprecated"]     = { strikethrough = true, sp = COLORS.diag_error },
+
+	-- tree-sitter
+	["@comment"]   = { fg = COLORS.comment, italic = true },
+	["@keyword"]   = { fg = COLORS.keyword },
+	["@string"]    = { fg = COLORS.string },
+	["@number"]    = { fg = COLORS.number },
+	["@boolean"]   = { fg = COLORS.boolean },
+	["@constant"]  = { fg = COLORS.constant },
+	["@type"]      = { fg = COLORS.fg },
+	["@operator"]  = { fg = COLORS.operator },
+	["@variable"]  = { fg = COLORS.variable },
+	["@function"]  = { fg = COLORS.Function },
+	["@method"]    = { fg = COLORS.Function },
+
+	-- lua
+	["@constructor.lua"] = { fg = COLORS.fg },
+	["@lsp.typemod.function.declaration.lua"] = { fg = COLORS.func_dec },
+
+	-- java
+	["javaExternal"] = { fg = COLORS.keyword },
+	["javaAnnotation"] = { fg = COLORS.metadata },
+	["@lsp.type.annotation.java"] = { fg = COLORS.metadata },
+	["@lsp.type.modifier.java"] = { fg = COLORS.keyword },
+	["@lsp.typemod.property.readonly.java"] = { fg = COLORS.constant },
+	["@lsp.typemod.method.declaration.java"] = { fg = COLORS.func_dec },
+	["@lsp.typemod.class.constructor.java"] = { fg = COLORS.func_dec },
+	["@lsp.type.namespace.java"] = { fg = COLORS.fg },
+}
+
+for k, v in pairs(fields) do
+	vim.api.nvim_set_hl(0, k, v)
+end
