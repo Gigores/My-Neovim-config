7885976bfb2afe46e3daa47aee970fb6f8e77d7e
Priklen <igorsamodumskij@gmail.com>
2026-05-19 14:41:32 +0300

    add annotations to providers

--- lua/util/providers.lua
+++ lua/util/providers.lua
@@ -1,5 +1,7 @@
 local M = {}
 
+---@param repo string  # example: `Gigores/My-Neovim-config`
+---@return string  # a full repo URL
 function M.github(repo)
 	assert(type(repo) == "string", "providers.github: \"repo\" should be a string, got " .. type(repo))
 	return "https://github.com/" .. repo
